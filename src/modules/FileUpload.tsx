51eb41b3f07b16be3b8be34a3518398f95ca853c
Vlad <hrinyov@gmail.com>
2023-07-08 23:32:31 +0300

    add metadata reading

--- src/modules/FileUpload.tsx
+++ src/modules/FileUpload.tsx
@@ -1,11 +1,35 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState} from "react";
 import { useDropzone } from "react-dropzone";
+import exifr from "exifr";
 
 const FileUpload: React.FC = () => {
+    const [file, setFile] = useState<File | null>(null);
+    const [metadata, setMetadata] = useState<any | null>(null);
+
   const onDrop = useCallback((acceptedFiles: File[]) => {
     // Handle the uploaded files here
-    console.log(acceptedFiles);
+    setFile(acceptedFiles[0]);
+    extractMetadata(acceptedFiles[0]);
+    console.log(metadata);
   }, []);
+
+const extractMetadata = async (file: File) => {
+  const reader = new FileReader();
+  reader.onload = async (event) => {
+    const dataURL = event.target?.result as string | undefined;
+    if (dataURL) {
+      try {
+        const metadata = await exifr.parse(dataURL);
+        setMetadata(metadata);
+      } catch (error) {
+        console.log("Error extracting metadata:", error);
+      }
+    }
+  };
+  reader.readAsDataURL(file);
+};
+
+
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
@@ -17,6 +41,12 @@ const FileUpload: React.FC = () => {
       ) : (
         <p>Drag and drop files here, or click to select files</p>
       )}
+      
+        <div>
+          <h2>Metadata:</h2>
+          <pre>{JSON.stringify(metadata, null, 2)}</pre>
+        </div>
+      
     </div>
   );
 };
