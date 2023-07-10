00dd9e450e765e6c6951ef9fd1b17f206664647a
Vlad <hrinyov@gmail.com>
2023-07-11 00:11:25 +0300

    add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 import FileUpload from './modules/FileUpload';
+import DataDisplay from './modules/DataDisplay';
 
 function App() {
-  
+ const [sharedData, setSharedData] = useState("");
+ const updateSharedData = ( newData: any ): void => {
+   return  setSharedData(newData);
+ };
 
   return (
     <>
       <h1>File Upload Example</h1>
-      <FileUpload />
+      <FileUpload updateSharedData = {updateSharedData} />
+      <DataDisplay data={sharedData} />
     </>
   );
 }
