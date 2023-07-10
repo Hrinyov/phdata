f9ff955eac9e368de6691819df6e6ce68cd2bab9
Vlad <hrinyov@gmail.com>
2023-07-11 00:13:05 +0300

    add method to download info in jpg

--- src/modules/DataDisplay.tsx
+++ src/modules/DataDisplay.tsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useRef } from "react";
+import * as htmlToImage from "html-to-image";
 
 interface DataDisplayProps {
   data: any;
 }
 
 const DataDisplay: React.FC<DataDisplayProps> = ({ data }) => {
-  console.log(data);
+  const dataDisplayRef = useRef<HTMLDivElement>(null);
+
+  const captureScreenshot = async () => {
+    if (dataDisplayRef.current) {
+      const image = await
+      htmlToImage
+        .toJpeg(dataDisplayRef.current)
+       downloadScreenshot(image)
+    }
+  };
+
+  const downloadScreenshot = (screenshot: string) => {
+    const link = document.createElement("a");
+    link.href = screenshot;
+    link.download = "screenshot.jpg";
+    link.click();
+  };
+
   return (
-    <div>
-      <div>ISO: {data.ISO} </div>
+  <>
+  <div ref={dataDisplayRef} className="info">
+      <div>ISO: {data.ISO}</div>
       <div>F{data.FNumber}</div>
-      <div>ExposureTime {data.ExposureTime}</div>
+      <div>ExposureTime: {data.ExposureTime}</div>
     </div>
+    <div>
+    <button onClick={captureScreenshot}>Capture Screenshot</button>  
+    </div>
+  </>
+    
   );
 };
 
