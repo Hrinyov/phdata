68345ad80ff00360a5e04cb2bccc2d5a1be2810a
Vlad <hrinyov@gmail.com>
2023-07-11 00:13:50 +0300

    fix

--- src/modules/FileUpload.tsx
+++ src/modules/FileUpload.tsx
@@ -1,39 +1,42 @@
 import React, { useCallback, useState} from "react";
 import { useDropzone } from "react-dropzone";
 import exifr from "exifr";
-import DataDisplay from "./DataDisplay";
 
-const FileUpload: React.FC = () => {
-    const [file, setFile] = useState<File | null>(null);
-    const [metadata, setMetadata] = useState<any | null>(null);
+interface FileUploadProps {
+  updateSharedData: (arg: DataTransfer) => void
+}
+
+const FileUpload: React.FC<FileUploadProps> = (  { updateSharedData }  ) => {
+  const [file, setFile] = useState<File | null>(null);
+  const [metadata, setMetadata] = useState<any | null>(null);
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
     // Handle the uploaded files here
     setFile(acceptedFiles[0]);
     extractMetadata(acceptedFiles[0]);
-    console.log(metadata);
+
   }, []);
 
-const extractMetadata = async (file: File) => {
-  const reader = new FileReader();
-  reader.onload = async (event) => {
-    const dataURL = event.target?.result as string | undefined;
-    if (dataURL) {
-      try {
-        const metadata = await exifr.parse(dataURL);
-        setMetadata(metadata);
-      } catch (error) {
-        console.log("Error extracting metadata:", error);
+  const extractMetadata = async (file: File) => {
+    const reader = new FileReader();
+    reader.onload = async (event) => {
+      const dataURL = event.target?.result as string | undefined;
+      if (dataURL) {
+        try {
+          const metadata = await exifr.parse(dataURL);
+          setMetadata(metadata);
+          updateSharedData(metadata);
+        } catch (error) {
+          console.log("Error extracting metadata:", error);
+        }
       }
-    }
+    };
+    reader.readAsDataURL(file);
   };
-  reader.readAsDataURL(file);
-};
-
-
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
+  
   return (
     <div {...getRootProps()}>
       <input {...getInputProps()} />
@@ -42,7 +45,6 @@ const extractMetadata = async (file: File) => {
       ) : (
         <p>Drag and drop files here, or click to select files</p>
       )}
-      {file && metadata && <DataDisplay data={metadata} />}
     </div>
   );
 };
