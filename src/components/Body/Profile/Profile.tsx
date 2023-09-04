40bc3d0625b76f3df651ab8bb05d8bcc3f6bb88f
Vlad <hrinyov@gmail.com>
2023-09-04 23:42:55 +0300

    use the use-token hook

--- src/components/Body/Profile/Profile.tsx
+++ src/components/Body/Profile/Profile.tsx
@@ -1,16 +1,19 @@
 import React, {useState} from "react"
 import Login from "../../Login/Login";
-
+import useToken from "../../../hooks/use-token";
 
 
 const Profile: React.FC = () => {
     const [loginIsShow, setLoginIsShow] = useState(false);
+    const {token, setToken} = useToken();
+
     const closeLogin =() =>{
         setLoginIsShow(false);
     }
+
     if(true) {
         return <>
-        {loginIsShow && <Login onClose={closeLogin}/>}
+        {loginIsShow && <Login onClose={closeLogin} onSetToken={setToken}/> }
         <h3>Please login</h3>
         <button onClick={()=>setLoginIsShow(true)}>Login</button>
         </>
