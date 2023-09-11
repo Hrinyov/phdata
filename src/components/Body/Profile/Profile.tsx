cafd5de99e094627498a22eedf5281782d1a1b06
Vlad <hrinyov@gmail.com>
2023-09-12 00:11:17 +0300

    add posts get method

--- src/components/Body/Profile/Profile.tsx
+++ src/components/Body/Profile/Profile.tsx
@@ -1,11 +1,43 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import Login from "../../Login/Login";
 import useToken from "../../../hooks/use-token";
+import axios from "axios";
+import Posts from "./Posts/Posts";
 
 
 const Profile: React.FC = () => {
     const [loginIsShow, setLoginIsShow] = useState(false);
     const {token, setToken} = useToken();
+    const [posts, setPosts] = useState();
+
+    useEffect(()=>{
+        const init = async () =>{
+        if(!token){
+            return
+        }
+        if(token){
+            try {
+            const response = await axios.get("http://localhost:8080/gallery", {
+              headers: {
+                "x-auth-token": token,
+              },
+            });
+            if (response.status === 200) {
+              const { posts } = response.data;
+              setPosts(posts);
+            }
+            if (response.status === 511) {
+                setToken(null);
+                setLoginIsShow(true);
+            }    
+            } catch (error) {
+               console.error('Get posts failed')
+            }    
+        }  
+        }
+        init()
+    },[]);
+
 
     const closeLogin =() =>{
         setLoginIsShow(false);
@@ -27,8 +59,7 @@ const Profile: React.FC = () => {
             {token && (
               <>
                 <button onClick={logOutHandler}>Log out</button>
-                <h2>Photo gallery</h2>
-                <span>some data</span>
+                <Posts posts={posts}/>
               </>
             )}
         </>
