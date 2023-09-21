12592fdfe53a7de3f7b6fa385f96fc4ffd46ba9e
Vlad <hrinyov@gmail.com>
2023-09-21 19:49:28 +0300

    fix function logic

--- src/utils/NumberConverter.ts
+++ src/utils/NumberConverter.ts
@@ -1,6 +1,29 @@
 
 const NumberConverter= (number: string | undefined) => {
-  return "1/15";
-};
+  if(number){
+    const decimal = +number;
+    if (decimal >= 1) {
+      return `${decimal}"`;
+    }
+    if (decimal > 0 || decimal < 1) {
+      const maxDenominator = 10000;
+      const numerator = 1;
+      let denominator = 1;
+      let error = Math.abs(decimal - numerator / denominator);
+
+    for (let i = 2; i <= maxDenominator; i++) {
+      const newDenominator = i;
+      const newError = Math.abs(decimal - numerator / newDenominator);
+
+      if (newError < error) {
+        denominator = newDenominator;
+        error = newError;
+      }
+    }
+    return `${numerator}/${denominator}`;
+    }
+  } 
+  return 'empty';
+}
 
 export default NumberConverter;
